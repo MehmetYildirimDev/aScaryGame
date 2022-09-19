50b1074ac5ece7c4d8b3e79ebaab2f82a668b67b
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-09-19 16:17:33 +0300

    feat : create field of view for enemy

--- Assets/Scripts/Enemy/EnemyGhoul.cs
+++ Assets/Scripts/Enemy/EnemyGhoul.cs
@@ -21,12 +21,22 @@ public class EnemyGhoul : Enemy
     public AudioClip[] grassClips = default;
     public float footStepTimer = 0;
 
+
+    [Header("Field of View")]
+    public float FovRadius;
+    [Range(0, 360)] public float FovAngle;
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    public bool canSeePlayer;
+
+
     private void Start()
     {
         target = FirstPersonController.instance.transform;
         Agent = GetComponent<NavMeshAgent>();
         audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+        StartCoroutine(FovRuotine());
     }
 
 
@@ -65,16 +75,16 @@ public class EnemyGhoul : Enemy
         //normalized: yonunu koruyo ama uzunlugu 1 kaliyo ///mesela burada yon onemli o yuzden yaptik
 
         Quaternion LookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation,LookRotation,Time.deltaTime*5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, LookRotation, Time.deltaTime * 5f);
 
     }
 
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, lookRadius);
+    //}
     public void HandleFootSteps()
     {
 
@@ -111,4 +121,41 @@ public class EnemyGhoul : Enemy
 
     }
 
+    private IEnumerator FovRuotine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FOVChechk();
+
+        }
+
+    }
+
+    private void FOVChechk()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, FovRadius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directonToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directonToTarget) < FovAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directonToTarget, distanceToTarget, obstructionMask))
+                    canSeePlayer = true;
+                else
+                    canSeePlayer = false;
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer)//daha once gorduyse býrakýyo
+            canSeePlayer = false;
+    }
 }
