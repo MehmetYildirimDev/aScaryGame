92adb8972fcb758ac8b916255edd95308acd59c0
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-09-29 19:26:49 +0300

    feat: create radgoll for war zombie

---
+++ Assets/Scripts/EnemyWarZombie.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWarZombie : Enemy
+{
+    private void Update()
+    {
+
+        if (useFootSteps)
+            HandleFootSteps();
+
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance <= lookRadius)
+        {
+            Agent.isStopped = false;
+            animator.SetBool("near", true);
+
+            Agent.SetDestination(target.position);
+
+            base.FaceTarget();
+        }
+        else
+        {
+           animator.SetBool("near", false);
+
+            Agent.isStopped = true;
+        }
+
+
+        //if (distance <= Agent.stoppingDistance)
+        //{
+        //    FaceTarget();
+        //}
+
+        animator.SetFloat("isStop", distance);
+
+    }
+
+
+
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, lookRadius);
+    //}
+}
